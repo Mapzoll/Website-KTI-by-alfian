934c862648ce6f1d92aed0532818e1a2577c9d1d
M Alfian Pratama <ian25pratama@gmail.com>
2025-03-05 22:59:28 +0700

    rapihin navbar, menu, bg, dan header

--- Website KTI/dist/js/script.js
+++ Website KTI/dist/js/script.js
@@ -1,7 +1,18 @@
+// Hamburger menu
 const btnHamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".menu");
 
 btnHamburger.addEventListener("click", () => {
   btnHamburger.classList.toggle("is-active");
   menu.classList.toggle("menu-active");
+});
+
+// Tutup di luar sidebar tanpa harus klik hamburger icon
+const hamburger = document.querySelector(".hamburger");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !menu.contains(e.target)) {
+    menu.classList.remove("menu-active");
+    btnHamburger.classList.remove("is-active");
+  }
 });
